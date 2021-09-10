7903ab07727ac54446d289957cbba3f8ca78b837
Julian <juliandarruiz@gmail.com>
2021-09-10 20:59:00 -0300

    Programa finalizado.

---
+++ tpGIT.php
@@ -0,0 +1,14 @@
+<?php
+$nombre= $_POST['nombre'];
+$apellido=$_POST['apellido'];
+$telefono=$_POST['telefono'];
+$dni=$_POST['dni'];
+
+
+echo "$nombre $apellido ha sido registrado/a correctamente!";
+
+
+
+
+
+
